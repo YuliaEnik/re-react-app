78a32b599b936e68b6b54889e10672841cb61cec
Yuliya Aleinik <aleinikya@mail.ru>
2025-02-03 01:32:50 +0300

    feat: add search button

--- src/Components/Search/index.tsx
+++ src/Components/Search/index.tsx
@@ -11,20 +11,13 @@ class Search extends React.Component<searchProps, IState> {
       value: '',
     };
     this.handleChange = this.handleChange.bind(this);
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
   }
 
-  handleFormSubmit(e: React.SyntheticEvent) {
-    e.preventDefault();
-    if (e) {
-      const target = e.target as typeof e.target & {
-        text: { value: string };
-      };
-      const value = target.text.value.trim();
-      localStorage.setItem('items', value);
-      this.props.onSearch(this.state.value.trim());
-    }
-  }
+  handleClick = () => {
+    const value = this.state.value.trim();
+    localStorage.setItem('items', value);
+    this.props.onSearch(this.state.value.trim());
+  };
 
   handleChange: React.ChangeEventHandler<HTMLInputElement> = (event) => {
     this.setState({ value: event.target.value });
@@ -49,17 +42,15 @@ class Search extends React.Component<searchProps, IState> {
     return (
       <header className="header">
         <div className="search">
-          <form onSubmit={this.handleFormSubmit} className="search-form">
-            <label htmlFor="serchInput"></label>
-            <input
-              type="text"
-              name="text"
-              className="search-form_input"
-              placeholder="Search..."
-              value={this.state.value}
-              onChange={this.handleChange}
-            />
-          </form>
+          <input
+            type="text"
+            name="text"
+            className="search-form_input"
+            placeholder="Search..."
+            value={this.state.value}
+            onChange={this.handleChange}
+          />
+          <button onClick={this.handleClick} className="search-button"></button>
         </div>
       </header>
     );
