f0c4bef6f621c6b325c47bb9e9c74d7eed37ba52
Yuliya Aleinik <aleinikya@mail.ru>
2025-05-05 11:50:25 +0300

    refactor: add deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   css: {
     preprocessorOptions: {
       scss: {
