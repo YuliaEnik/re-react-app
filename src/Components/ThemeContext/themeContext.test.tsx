21724062d970039a4f70bd79174b3042c188cab1
Yuliya Aleinik <aleinikya@mail.ru>
2025-05-05 11:56:36 +0300

    refactor: add deploy

--- src/Components/ThemeContext/themeContext.test.tsx
+++ src/Components/ThemeContext/themeContext.test.tsx
@@ -1,8 +1,8 @@
 import '@testing-library/jest-dom/vitest';
 import { describe, it, expect } from 'vitest';
 import { fireEvent, render, screen } from '@testing-library/react';
-import { ThemeProvider, ThemeContext } from './themeContext';
 import { useContext } from 'react';
+import { ThemeContext, ThemeProvider } from './themeContext';
 
 describe('ThemeProvider Component', () => {
   it('provides the initial theme as "light"', () => {
