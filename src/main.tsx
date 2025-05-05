067e4159cc9dc2a71057288808f1a08dfaf2e85d
Yuliya Aleinik <aleinikya@mail.ru>
2025-05-05 16:33:31 +0300

    refactor: deploy

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App.tsx';
 import ErrorBoundary from './Components/ErrorBoundary';
-import { createBrowserRouter, RouterProvider } from 'react-router';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router';
 import { Provider } from 'react-redux';
 import { NotFound } from './view/NotFound/index.tsx';
 import { store } from './Store/store.tsx';
@@ -11,22 +11,21 @@ import { store } from './Store/store.tsx';
 //const isProduction = process.env.NODE_ENV === 'production';
 //const basename = isProduction ? '/re-react-app' : '/';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <App />,
-      errorElement: <NotFound />,
-    },
-    {
-      path: '*',
-      element: <NotFound />,
-    },
-  ],
+const router = createBrowserRouter([
   {
-    basename: '/re-react-app',
-  }
-);
+    path: '/',
+    element: <Navigate to={`/re-react-app`} />,
+  },
+  {
+    path: '/re-react-app',
+    element: <App />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
+  },
+]);
 
 createRoot(document.getElementById('root') as Element).render(
   <StrictMode>
